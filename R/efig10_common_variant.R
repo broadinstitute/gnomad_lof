3458efcebfb96c36d8c2b53fac63d96d84a88afa
konradjk <konradjkarczewski@gmail.com>
2019-10-07 21:18:37 -0400

    fixing metagen tidyr interaction issue in ef10

--- R/efig10_common_variant.R
+++ R/efig10_common_variant.R
@@ -29,19 +29,24 @@ tau_star = function(save_plot=F, normalize=F) {
 }
 
 enrichment_corrections = function(save_plot=F) {
-  
-  enrichment_correction_data = sum_stats %>%
+
+  filt_sum_stats = sum_stats %>%
     filter(cond %in% c('all100', 'condbrain', 'condgenesize') &
              grepl('oe_lof_upper_quantile_', name) &
              (is.na(cor_rm0.2) | cor_rm0.2 == 0)  # Select uncorrelated variables
-    ) %>%
-    group_by(name, cond) %>%
-    summarise(meta_enrichment = metagen(taustar, se)$TE.random,
-              meta_sd = metagen(taustar, se)$seTE.random) %>%
+    )
+  enrichment_correction_data = ddply(filt_sum_stats, c('name', 'cond'), function(x) {
+    enrichment = x$enrichment
+    enrichment_SE = x$enrichment_SE
+    meta_enrichment = metagen(enrichment, enrichment_SE)$TE.random
+    meta_sd = metagen(enrichment, enrichment_SE)$seTE.random
+    return(data.frame(meta_enrichment, meta_sd))
+  }) %>%
     mutate(decile = as.integer(str_sub(name, -1)),
            cond=fct_recode(cond, 'Baseline' = 'all100', 
                            'Baseline + brain\nexpression' = 'condbrain', 
-                           'Baseline + gene size +\nexon count' = 'condgenesize'))
+                           'Baseline + gene size +\nexon count' = 'condgenesize')
+    )
   
   p = enrichment_correction_data %>%
     ggplot + aes(x = decile, y = meta_enrichment, 
@@ -52,7 +57,7 @@ enrichment_corrections = function(save_plot=F) {
     oe_x_axis + facet_wrap(~cond) #+ theme(panel.spacing = unit(0.1, "lines"))
   
   if (save_plot) {
-    pdf('e9b_enrichment_corrections.pdf', height=3, width=4)
+    pdf('e10b_enrichment_corrections.pdf', height=3, width=4)
     print(p)
     dev.off()
   }
@@ -60,19 +65,25 @@ enrichment_corrections = function(save_plot=F) {
 }
 
 window_assessment = function(save_plot=F) {
-  window_data = sum_stats %>%
+  filt_data = sum_stats %>%
     filter(cond %in% c('all100', 'all50', 'all10') &
              grepl('oe_lof_upper_quantile_', name) &
              (is.na(cor_rm0.2) | cor_rm0.2 == 0)  # Select uncorrelated variables
-    ) %>%
-    group_by(name, cond) %>%
-    summarise(meta_enrichment = metagen(enrichment, se)$TE.random,
-              meta_sd = metagen(enrichment, se)$seTE.random) %>%
+    )
+  
+  window_data = ddply(filt_data, c('name', 'cond'), function(x) {
+    enrichment = x$enrichment
+    enrichment_SE = x$enrichment_SE
+    meta_enrichment = metagen(enrichment, enrichment_SE)$TE.random
+    meta_sd = metagen(enrichment, enrichment_SE)$seTE.random
+    return(data.frame(meta_enrichment, meta_sd))
+  }) %>%
     mutate(decile = as.integer(str_sub(name, -1)),
            cond=fct_recode(cond, '100 kb' = 'all100',
                            '50 kb' = 'all50', 
                            '10 kb' = 'all10'),
-           cond=fct_relevel(cond, '100 kb', '50 kb', '10 kb'))
+           cond=fct_relevel(cond, '100 kb', '50 kb', '10 kb')
+    )
   
   p = window_data %>%
     ggplot + aes(x = decile, y = meta_enrichment, col = cond,
